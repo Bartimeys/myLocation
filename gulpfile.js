2fdef34a25ed9ba72440d4d2bb92e7370feecb7e
Bartimeus <bartimeusurucskiy@gmail.com>
2017-12-17 21:47:51 +0200

    added browser-sync

--- gulpfile.js
+++ gulpfile.js
@@ -7,6 +7,7 @@ var del = require('del');
 var minifyCSS = require('gulp-csso');
 var sass = require('gulp-sass');
 var autoprefixer = require('gulp-autoprefixer');
+var browserSync = require('browser-sync').create();
 
 var paths = {
     scripts: ['src/js/**/*.js', 'src/js/**/*.js'],
@@ -14,6 +15,8 @@ var paths = {
     css : ['src/css/*.scss', 'node_modules/materialize-css/dist/css/materialize.min.css'],
     fonts : ['node_modules/materialize-css/dist/fonts/**/**']
 };
+
+
 
 // Not all tasks need to use streams
 // A gulpfile is just another node program and you can use any package available on npm
@@ -30,8 +33,13 @@ gulp.task('scripts', ['clean'], function() {
         .pipe(uglify())
         .pipe(concat('all.min.js'))
         .pipe(sourcemaps.write())
-        .pipe(gulp.dest('build/js'));
+        .pipe(gulp.dest('build/js'))
+        .pipe(browserSync.reload({
+            stream: true
+        }))
+
 });
+
 //concat, buils, autoprefix scss
 gulp.task('css',['clean'], function(){
     return gulp.src(paths.css)
@@ -44,6 +52,9 @@ gulp.task('css',['clean'], function(){
         .pipe(minifyCSS())
         .pipe(concat('all.style.css'))
         .pipe(gulp.dest('build/css'))
+        .pipe(browserSync.reload({
+            stream: true
+        }))
 });
 //copy fonts
 gulp.task('font', ['clean'], function() {
@@ -59,14 +70,23 @@ gulp.task('images', ['clean'], function() {
         .pipe(imagemin({optimizationLevel: 5}))
         .pipe(gulp.dest('build/img'));
 });
+// Configure the browserSync task
+gulp.task('browserSync', function() {
+    browserSync.init({
+        server: {
+            baseDir: ''
+        }
+    })
+});
 
 // Rerun the task when a file changes
 gulp.task('watch', function() {
-    gulp.watch(paths.scripts, ['scripts']);
-    gulp.watch(paths.images, ['images']);
-    gulp.watch('css/*.scss', ['sass']);
+    gulp.watch(paths.scripts, ['scripts'],browserSync.reload);
+    gulp.watch(paths.images, ['images'],browserSync.reload);
+    gulp.watch('css/*.scss', ['sass'],browserSync.reload);
     gulp.watch(paths.fonts, ['font']);
+    gulp.watch('*.html', browserSync.reload);
 });
 
 // The default task (called when you run `gulp` from cli)
-gulp.task('default', ['watch', 'scripts', 'images','css','font']);
+gulp.task('default', ['browserSync','watch', 'scripts', 'images','css','font']);
